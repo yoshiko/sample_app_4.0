46adaba6e37af3edecda270712457d441c2ac8a3
maasa <maasa.giss2@gmail.com>
2014-05-10 15:15:51 +0900

    Usersコントローラ追加

---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe "UserPages" do
+	subject { page }
+
+	describe "signup page" do
+		before { visit signup_path }
+
+		it { is_expected_to.have_content('Sign up') }
+		it { is_expected_to.have_title(full_title('Sign up')) }
+  end
+end
